88a5cfe0ab89bb92ee29e58323c7c51b9299aa22
kimminhyeok13 <kimminhyeok13@gmail.com>
2019-03-25 21:00:09 +0900

    home

--- programming_3.py
+++ programming_3.py
@@ -32,27 +32,45 @@ def bubble_sort(sort_list,size):
 	return time.time()-start_time
 
 
+def selection_sort(sort_list,size):
+	index=-1
+	start_time=time.time()
+	for i in range(0,size):
+		select=sort_list[0]
+		for j in ragne(1,size-i):
+			if(sort_list[j]>select):
+				select=sort_list[j]
+				index=j
+		swap(sort_list,j,size-i)
+	return time.time()-start_time
+
+
+def insertion_sort(sort_list,size):
+	start_time=time.time()
+	for i in range(1,size):
+		for j in range(i)
+
 
 def main():
-	size_list=[1000,10000,100000]
-	sort_list=[]
-	time_list=[]
-	print("		Random1000		Reverse1000 	Random10000 	Reverse10000 	Random100000 	Reverse100000")
-	print("Bubble",end='')
-	for size in size_list: 
-		sort_list=input_random(size)
-		rand_bubble_1000=bubble_sort(sort_list,size)
-#		print(sort_list)
-#		print("랜덤 버블정렬 실행시간")
-		print("		%0.3f"%rand_bubble_1000)
+# 	size_list=[1000,10000,100000]
+# 	sort_list=[]
+# 	time_list=[]
+# 	print("		Random1000		Reverse1000 	Random10000 	Reverse10000 	Random100000 	Reverse100000")
+# 	print("Bubble",end='')
+# 	for size in size_list: 
+# 		sort_list=input_random(size)
+# 		rand_bubble_1000=bubble_sort(sort_list,size)
+# #		print(sort_list)
+# #		print("랜덤 버블정렬 실행시간")
+# 		print("		%0.3f"%rand_bubble_1000)
 		
-		sort_list=input_reverse(size)
-		reverse_bubble_1000=bubble_sort(sort_list,size)
-#		print(sort_list)
-		print("		%0.3f"%reverse_bubble_1000)
+# 		sort_list=input_reverse(size)
+# 		reverse_bubble_1000=bubble_sort(sort_list,size)
+# #		print(sort_list)
+# 		print("		%0.3f"%reverse_bubble_1000)
 
-#		print("역순 버블정렬 실행 시간 :%0.2f" % reverse_bubble_1000)
-		size*=10
+# #		print("역순 버블정렬 실행 시간 :%0.2f" % reverse_bubble_1000)
+# 		size*=10
 main()
 
 
